04d1029abbb54a5c2256c0ab46b2783af6a5466e
Dave Boehm <davidnboehm@gmail.com>
2018-02-24 12:14:16 -0600

    dropdown component

---
+++ src/App.vue
@@ -0,0 +1,125 @@
+<template>
+    <div id="app">
+        <section class="section">
+            <h2>Standard Dropdown</h2>
+            <p>The dog you've chosen has a score of: {{dogScore}}</p>
+            <dropdown 
+                :data="dropdownItems" 
+                :display-key="'label'" 
+                :current-selection="'Please select'" 
+                @update="alertDogScore($event)" />
+        </section>
+        
+        <section class="section">
+            <h2>Dropdown with Sections</h2>
+            <p>The name you've chosen has a score of: {{nameScore}}</p>
+            <dropdown 
+                :sections="sectionedDropdownItems" 
+                :display-key="'name'" 
+                :current-selection="sectionedDropdownItems[1].data[2].name" 
+                @update="alertNameScore($event)" />
+        </section>
+        
+    </div>
+</template>
+
+<script>
+    import Dropdown from './components/Dropdown.vue'
+    
+    export default {
+        name: 'app',
+        components: {
+            'dropdown': Dropdown
+        },
+        data() {
+            return {
+                dogScore: null,
+                nameScore: null,
+                sectionDropdown__isOpen: false,
+                dropdown__isOpen: false,
+                dropdownItems: [{
+                        label: 'Corgi',
+                        value: 1000
+                    },
+                    {
+                        label: 'Australian Shepard',
+                        value: 950
+                    },
+                    {
+                        label: 'Beagle',
+                        value: 900
+                    },
+                    {
+                        label: 'Labrador',
+                        value: 750
+                    },
+                    {
+                        label: 'Goldendoodle',
+                        value: 600
+                    }
+                ],
+                sectionedDropdownItems: [{
+                        sectionTitle: 'Girls',
+                        data: [{
+                                value: 100,
+                                name: 'Susie'
+                            },
+                            {
+                                value: 200,
+                                name: 'Debbie'
+                            },
+                            {
+                                value: 50,
+                                name: 'Patricia'
+                            }
+                        ]
+                    },
+                    {
+                        sectionTitle: 'Boys',
+                        data: [{
+                                value: 100,
+                                name: 'John'
+                            },
+                            {
+                                value: 200,
+                                name: 'Steve'
+                            },
+                            {
+                                value: 50,
+                                name: 'Harry'
+                            }
+                        ]
+                    }
+                ]
+            }
+        },
+        methods: {
+            alertDogScore($event) {
+                this.dogScore = $event.value;
+                this.sectionDropdown__isOpen = false;
+            },
+            alertNameScore($event) {
+                this.nameScore = $event.value;
+                this.dropdown__isOpen = false;
+            }
+        }
+    }
+</script>
+
+<style>
+    .section {
+        width: 100%;
+        max-width: 600px;
+        margin: 40px auto;
+    }
+    h2 {
+        font-family: 'Avenir', 'Helvetica Nueue', sans-serif;
+        font-size: 20px;
+        margin: 20px 0;
+    }
+    p {
+        font-family: 'Avenir', 'Helvetica Nueue', sans-serif;
+        font-size: 14px;
+        margin: 20px 0;
+    }
+</style>
---
+++ src/components/Dropdown.vue
@@ -0,0 +1,244 @@
+<template>
+    <div class="dropdown" :class="{ active: isOpen }" v-on-clickaway="away" @keyup.esc="close()">
+        <button class="dropdown__button" tabindex="0" v-text="selectedItem[displayKey] || selectedItem" @click.stop="toggle"></button>
+        <i class="dropdown__caret"></i>
+
+        <template v-if="hasSections && isOpen">
+            <div class="dropdown__section">
+                <ul v-for="(section, idx) in sections"
+                    :key="idx"
+                    :class="{active: isOpen}"
+                    class="dropdown__ul">
+                <li class="dropdown__sectionTitle">
+                <b>{{ section.sectionTitle }}</b>
+                </li>
+                <li class="dropdown__li"
+                    tabindex="0"
+                    :key="i"
+                    v-for="(item, i) in section.data"
+                    :class="{ active: selectedItem == item}"
+                    v-text="displayKey === undefined ? item : item[displayKey]"
+                    @click.prevent.stop="update(item)"></li>
+                </ul>
+            </div>
+        </template>
+
+        <template v-if="!hasSections && isOpen">
+            <ul class="dropdown__ul" :class="{active: isOpen}">
+                <li class="dropdown__li" 
+                    tabindex="0" 
+                    :key="i" v-for="(item, i) in data" 
+                    :class="{ active: selectedItem == item.id}" 
+                    v-text="displayKey === undefined ? item : item[displayKey]" 
+                    @click.prevent.stop="update(item)"></li>
+            </ul>
+        </template>
+</div>
+</template>
+
+<script>
+    import {
+        mixin as clickaway
+    } from "vue-clickaway";
+    
+    export default {
+        name: "dropdown",
+        mixins: [clickaway],
+        props: {
+            data: {
+                type: Array,
+                default: () => ["No Items"]
+            },
+            displayKey: {
+                type: String
+            },
+            currentSelection: {
+                type: [Object, String]
+            },
+            sections: {
+                type: Array,
+                default: () => []
+            },
+            sectionTitle: {
+                type: String,
+                required: false
+            }
+        },
+        data() {
+            return {
+                isOpen: false,
+                selectedItem: this.currentSelection
+            };
+        },
+        computed: {
+            hasSections() {
+                return this.sections.length > 0;
+            }
+        },
+        methods: {
+            open() {
+                this.isOpen = true;
+            },
+            close() {
+                this.isOpen = false;
+            },
+            toggle($event) {
+                this.isOpen ? this.close() : this.open();
+            },
+            away($event) {
+                if (this.isOpen) this.close();
+            },
+    
+            update(item) {
+                this.selectedItem = item;
+                this.$emit("update", this.selectedItem);
+                this.close();
+            }
+        }
+    };
+</script>
+
+<style lang="scss">
+    $gray: #999;
+    $darker-gray: #555;
+
+    .dropdown {
+        box-sizing: border-box;
+        position: relative;
+        border: 1px solid #999;
+        border-radius: 4px;
+        cursor: pointer;
+        font-family: 'Avenir', 'Helvetica Nueue', sans-serif;
+        display: block;
+    }
+    
+    .dropdown__button {
+        border: 0 none;
+        padding: 8px 25px 8px 12px;
+        font-size: 15px;
+        line-height: 1.53;
+        color: #555;
+        background-color: transparent;
+        width: 100%;
+        white-space: nowrap;
+        text-align: left;
+        cursor: pointer;
+        box-sizing: border-box;
+    }
+    
+    .dropdown__caret {
+        font-size: 10px;
+        color: #555;
+        position: absolute;
+        top: 50%;
+        right: 10px;
+        pointer-events: none;
+        display: inline-block;
+        width: 0;
+        height: 0;
+        margin-left: 2px;
+        vertical-align: middle;
+        border-top: 4px dashed;
+        border-top: 4px solid\9;
+        border-right: 4px solid transparent;
+        border-left: 4px solid transparent;
+    }
+    
+    .dropdown__ul {
+        padding: 0;
+        margin: 8px 0 0 0;
+        list-style-type: none;
+        background-color: #fff;
+        box-sizing: border-box;
+        box-shadow: 0 0 10px rgba(0,0,0,0.25);
+        position: absolute;
+        top: 100%;
+        display: block;
+        width: 100%;
+        z-index: 10;
+    }
+    
+    .dropdown__section {
+        margin: 8px 0 0 0;
+        background-color: #fff;
+        box-shadow: 0 0 10px rgba(0,0,0,0.25);
+        box-sizing: border-box;
+        position: absolute;
+        top: 100%;
+        left: 0;
+        width: 100%;
+        z-index: 10;
+        .dropdown__ul {
+            position: static;
+            box-shadow: 0 0 0 0;
+            padding: 0 0 20px;
+            list-style-type: none;
+            &+.dropdown__ul {
+                margin-top: 0;
+                padding-top: 20px;    
+                border-top: 1px solid #eee;
+            }
+        }
+        .dropdown__li {
+            font-weight: normal;
+            padding: 3px 20px;
+        }
+    }
+    
+    .dropdown__sectionTitle {
+        padding: 3px 20px;
+        margin-bottom: 5px;
+    }
+    
+    .dropdown__li {
+        padding: 8px 10px;
+        text-align: left;
+        font-size: 14px;
+        color: #555;
+        &.active {
+            font-weight: bold;
+        }
+    }
+</style>
+
+<usage>
+// Standard Dropdown
+
+<dropdown :data="templateList"
+:display-key="'label'"
+:current-selection="getFormModel.template"
+@update='updateTemplate($event)'></dropdown>
+
+# Where 'data' is an array of values. Can be array of strings, or objects with a string property to display.
+# 'display-key' is the key name to display in the dropdown if the data prop is an array of objects
+# 'current-selection' is the value to be displayed in the dropdown button, ie the value to be PUT/POST'ed
+# @update event triggered whenever an item is clicked in the dropdown
+
+// Sectioned Dropdown
+
+<dropdown :sections="sectionedCategoryList"
+:display-key="'name'"
+:current-selection="getFormModel.category"
+@update='updateCategory($event)'></dropdown>
+
+# 'sections' replaces 'data' as that data source. This is an array of objects containing a 'sectionTitle' property and 'data' property. The nested data property works the same as above, but iterates under its section in the dropdown
+# Example:
+sectionedCategoryList: [
+    {
+        sectionTitle: 'First Section',
+        data: [
+            {id: 1, name: 'Foo'},
+            {id: 2, name: 'Bar'},
+            {id: 3, name: 'Baz'}
+        ]
+    },
+    {
+        sectionTitle: 'Second Section',
+        data: [
+            {id: 10, name: 'X'},
+            {id: 20, name: 'Y'},
+            {id: 30, name: 'Z'}
+        ]
+    }
+]
+</usage>
